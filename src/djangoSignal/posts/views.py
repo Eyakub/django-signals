e4e2fe3e78d5d620cbda6ab3f9d594da80fd2550
eyakub <eyakubsorkar@gmail.com>
2019-08-18 02:21:47 +0600

    request_started signals

--- src/djangoSignal/posts/views.py
+++ src/djangoSignal/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.core.signals import request_finished
+from django.core.signals import request_finished, request_started
 from django.dispatch import receiver, Signal
 from .models import Post
 # Create your views here.
@@ -14,6 +14,10 @@ def home(request):
     request_counter_signal.send(sender=Post,timestamp='2019-01-10')
     return HttpResponse('Here is the response')
 
+# request started signal
+@receiver(request_started)
+def print_started(sender, **kwargs):
+    print('request started...')
 
 # after finishing the httpresponse it counted as finished
 @receiver(request_finished)
@@ -27,3 +31,12 @@ def post_counter_signal_receiver(sender, **kwargs):
     print(kwargs)
 
 
+    # def ready(self):
+    #     request_started.connect(self.print_started)
+    #     request_started.connect(self.print_finished)
+
+    # def print_started(self, sender, **kwargs):
+    #     print('got request')
+    
+    # def print_finished(self, sender, **kwargs):
+    #     print('finished request')
